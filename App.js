82a5f7843fed1413ad162906ebe4f377288b700f
Yovany1998 <61251212+Yovany1998@users.noreply.github.com>
2022-01-28 10:53:45 -0600

    Inicio del proyecto

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+  <>
+  <View style={styles.contenedor}>
+    <View style={styles.caja1}></View>
+    <View style={styles.caja2}></View>
+    <View style={styles.caja3}></View>
+    <View style={styles.caja4}></View>
+  </View>
+  </>
+   
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: 'cornflowerblue',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  caja1:{
+    padding: 20,
+    backgroundColor: 'navy'
+  },
+  caja2:{
+    padding: 20,
+    backgroundColor: 'yellow'
+  },
+  caja3:{
+    padding: 20,
+    backgroundColor: 'green'
+  },
+  caja4:{
+    padding: 20,
+    backgroundColor: 'teal'
+  },
+ 
+});
+
+export default App;
